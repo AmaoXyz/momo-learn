0d435390821dc75094b9e75a30bcc3d27e4fbb88
Amao <amao@bnngoo.com>
2018-06-20 17:06:14 +0800

    修改服务器代理 局域网可以访问

--- server/api.js
+++ server/api.js
@@ -13,25 +13,4 @@ router.post('/api/test',(req,res)=>{
     });
 });
 
-/************** 创建(create) 读取(get) 更新(update) 删除(delete) **************/
-
-// // 获取已有账号接口
-// router.post('/api/user/login',(req,res) => {
-//     // 通过模型去查找数据库
-//     models.Login.find({account: req.body.account, password: req.body.password}, (err,data) => {
-//         if (err) {
-//             // res.send(err);
-//             res.send({'status': 1001, 'message': 'The account is not existed!', 'data': err});
-//         } else {
-//             console.log(data);
-//             if (data.length > 0) {
-//                 res.send({'status': 1000, 'message': 'Login successfully!', 'data': data});
-//             } else {
-//                 res.send({'status': 1001, 'message': 'The account is not existed!', 'data': err});
-//             }
-//         }
-//     });
-// });
-
-
 module.exports = router;
