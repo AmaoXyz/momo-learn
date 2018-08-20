ca7dcff39dde0b234ac1dd98e061946d21a7f9f8
Amao <amao@bnngoo.com>
2018-08-20 18:00:37 +0800

    固定广告位

--- src/html/1243.js
+++ src/html/1243.js
@@ -1,7 +1,8 @@
-$(function(){
-  $('body').prepend(
-  	$("<div class='top'></div><div class='left'></div><div class='right'></div><div class='bottom'></div> "))
-	$('.top').css({"width":"1000px","height":"100px","background-color":'red',"top":"0px"})
-	$('.left').css({"width":"100px","height":"1000px","background-color":'black',"left":"100px"})
-	$('.top,.left,.right,.bottom').css("position","fixed")
-})
+document.writeln("<div class=\"wisb\" style=\"width:700px; height:200px;margin: 0 auto\";><script src=\"222.js\"><\/script><\/div>\r\n");
+// $(function(){
+//     $('body').prepend(
+//         $("<div class='top'></div><div class='left'></div><div class='right'></div><div class='bottom'></div> "))
+//     $('.top').css({"width":"1000px","height":"100px","background-color":'red',"top":"0px"})
+//     $('.left').css({"width":"100px","height":"1000px","background-color":'black',"left":"100px"})
+//     $('.top,.left,.right,.bottom').css("position","fixed")
+// })
---
+++ src/html/222.js
@@ -0,0 +1,5 @@
+$(function(){
+  $('.wisb').prepend(
+      $("<div style='width: 600px;height: 200px;margin: 0 auto'><img src = 'http://m1y210.yytejg.com/mobile/jrs/640x200/20180802shoulian200.gif'></img></div >")
+  )
+})
